ecfe3ea8ed4af578620d046ace7ce156e9b9bc9b
josserden <josserden@gmail.com>
2024-12-03 11:08:35 +0200

    fix: fixed pre-commit hook

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
-"use client";
+'use client';
 
-import React from "react";
+import React from 'react';
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="p-6 bg-white rounded-lg shadow-xl">
+    <div className="flex h-screen items-center justify-center bg-gray-100">
+      <div className="rounded-lg bg-white p-6 shadow-xl">
         <h1 className="text-2xl font-semibold text-gray-800">
           Hello, Tailwind!
         </h1>
+
         <p className="mt-4 text-gray-600">This is a Tailwind CSS page.</p>
       </div>
     </div>
